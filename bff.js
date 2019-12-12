0b57625de6b1d976913546872b528772e2a887cb
Brian Swisher <brian.swisher@getpostman.com>
2019-12-12 15:33:06 -0800

    [poc] scrape getpostman.com

--- bff.js
+++ bff.js
@@ -14,6 +14,13 @@ app.use((req, res, next) => {
 
 app.set('json spaces', 2);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.set('json spaces', 2);
+
 app.use('/', express.static(path.join(__dirname, '..', 'dist')));
 
 app.get('/pm', (req, res) => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const React = require('react');
 const ReactDOM = require('react-dom');
 
