8baedf1dd61eddd30f62329f9c548626c6757e85
Brian Swisher <brian.swisher@getpostman.com>
2019-12-12 15:33:06 -0800

    [poc] scrape getpostman.com

--- bff.js
+++ bff.js
@@ -1,23 +1,86 @@
 const Bundler = require('parcel-bundler');
 const express = require('express');
+const fetch = require('node-fetch');
 const path = require('path');
 
 const app = express();
 const bundler = new Bundler('src/index.html', {});
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.set('json spaces', 2);
+
 app.use('/', express.static(path.join(__dirname, '..', 'dist')));
 
-app.get('/api', (req, res) => {
-  // raw
-  const payload = ['back', 'end', 'for', 'front', 'end'];
+app.get('/pm', (req, res) => {
+  fetch('https://www.getpostman.com/')
+    .then(
+      (r) => {
+        r.text()
+          .then((data) => {
+            const title = data.split('</h1>').shift().split('>').pop();
+            const cta = data.split(title).pop().split('</p>').shift().split('>').pop();
+            const url = data.split(cta).pop().split('</a>').shift().split('href="').pop().split('"').shift();
+            const hero = data.substr(data.indexOf('<section')).substr(data.substr(data.indexOf('<section')).indexOf('src="') + 5).split('"').shift();
+            const statsList = data.split(hero).pop().split('</p>').map(item => item.split('>').pop()).slice(0, 6);
+            const stats = {
+              [statsList[1]]: statsList[0],
+              [statsList[3]]: statsList[2],
+              [statsList[5]]: statsList[4]
+            };
+            const describe = [];
 
-  // prep
-  const message = payload
-    .map(word => word.charAt(0).toUpperCase() + word.substr(1))
-    .join('');
+            let explaination = data.split('</h2>').shift().split('>').pop();
 
-  res.json({ message });
+            describe.push({
+              title: explaination,
+              body: data.split(`${explaination}</h2>`).pop().substr(data.split(`${explaination}</h2>`).pop().indexOf('>') + 1).split('<').shift()
+            });
+
+
+            explaination = data.split('</h3>').shift().split('>').pop();
+
+            describe.push({
+              title: explaination,
+              body: data.split(`${explaination}</h3>`).pop().split('</p>').shift().split('<p>').pop(),
+              url: data.substr(data.indexOf(`${explaination}</h3>`)).substring(data.substr(data.indexOf(`${explaination}</h3>`)).indexOf('href="') + 6).split('"').shift()
+            });
+
+            const features = [];
+
+            data.split('</h3>').forEach(item => {
+              let snippet = item.substr(item.indexOf('h3'));
+
+              snippet = snippet.substr(snippet.indexOf('>') + 1);
+
+              if ( snippet.split(/\r?\n/).length < 2 && snippet.indexOf('<') === -1 && snippet.indexOf('>') === -1 ) {
+                features.push(snippet);
+              }
+            });
+
+            features.slice(0, 5).forEach(title => {
+              describe.push({
+                title,
+                body: data.split(`${title}</h3>`).pop().substr(data.split(`${title}</h3>`).pop().indexOf('>') + 1).split('<').shift(),
+                url: data.substr(data.indexOf(`${title}</h3>`)).substring(data.substr(data.indexOf(`${title}</h3>`)).indexOf('href="') + 6).split('"').shift()
+              });
+            });
+
+            res.json({
+              title,
+              cta,
+              url,
+              hero,
+              stats,
+              describe
+            });
+          });
+      }
+    );
 });
 
 app.use(bundler.middleware());
--- src/index.js
+++ src/index.js
@@ -1,47 +1,192 @@
+require('dotenv').config();
+
 const React = require('react');
 const ReactDOM = require('react-dom');
-const unique = require('uniq');
 
 const { useState } = React;
+const HOST = 'https://getpostman.com';
+const API_HOST = process.env.API_HOST || '';
 
-const numbers = [1, 2, 2, 3, 4, 5, 5, 5, 6];
-
-const style = {
-  backgroundColor: 'green',
-  boxSizing: 'content-box',
-  color: 'white',
-  margin: 50,
-  padding: 50,
-  textAlign: 'center'
-};
-
-const Hello = ({ nums }) => {
+const PM = () => {
   const [payload, setPayload] = useState();
 
   if (!payload) {
-    fetch('/api')
+    fetch(`${API_HOST}/pm`)
       .then(response => response.json())
       .then(data => {
         setPayload(data);
       });
   }
 
+  function getDescriptionCards() {
+    const describe = payload && payload.describe;
+    const describeNodes =
+      describe &&
+      describe.map(item => {
+        const { body, title, url } = item;
+        const urlNode =
+          (url &&
+            React.createElement(
+              'a',
+              { className: 'link', href: `${HOST}${url}` },
+              'Learn More',
+              React.createElement(
+                'style',
+                null,
+                `
+          .link {
+            display: inline-block;
+            color: #FF6C37;
+            font-weight: bold;
+            padding: 20px 0 0;
+          }
+
+          .link:hover { color: red }
+        `
+              )
+            )) ||
+          null;
+
+        return React.createElement(
+          'div',
+          {
+            style: {
+              backgroundColor: 'white',
+              border: '1px solid #666',
+              borderRadius: 5,
+              color: '#666',
+              margin: '12px auto',
+              padding: '2px 30px 40px',
+              width: '60%'
+            }
+          },
+          React.createElement('h4', null, title),
+          React.createElement('div', {
+            dangerouslySetInnerHTML: { __html: body }
+          }),
+          urlNode
+        );
+      });
+
+    return describeNodes && React.createElement('div', null, ...describeNodes);
+  }
+
+  function getStatsCard() {
+    const stats = payload && payload.stats;
+    const statsKeys = (stats && Object.keys(stats)) || [];
+    const statsNodes = statsKeys.map(key => {
+      return React.createElement(
+        'div',
+        {
+          style: {
+            backgroundColor: '#666',
+            borderRadius: 5,
+            color: 'white',
+            display: 'inline-block',
+            margin: 10,
+            padding: '5px 10px'
+          }
+        },
+        `${key}: ${stats[key]}`
+      );
+    });
+
+    return React.createElement('div', null, ...statsNodes);
+  }
+
+  function getDownloadCard() {
+    const title = React.createElement(
+      'h1',
+      null,
+      (payload && payload.title) || ''
+    );
+    const cta = React.createElement(
+      'h2',
+      { style: { color: '#1C272B' } },
+      (payload && payload.cta) || ''
+    );
+    const hero = React.createElement('img', {
+      width: 400,
+      style: {
+        display: 'block',
+        margin: '0 auto 20px'
+      },
+      src: payload && `${HOST}${payload.hero}`
+    });
+    const downLoadBtn = React.createElement(
+      'button',
+      {
+        onClick: () => {
+          document.location.href = `${HOST}${payload.url}`;
+        },
+        className: 'download'
+      },
+      'Download',
+      React.createElement(
+        'style',
+        null,
+        `
+          .download {
+            background-color: #1C272B;
+            color: white;
+            cursor: pointer;
+            font-size: 20px;
+            border-radius: 10px;
+            margin-top: 20px;
+            padding: 10px 20px;
+          }
+
+          .download:hover { background-color: red }
+        `
+      )
+    );
+
+    const innerDownloadCard = React.createElement(
+      'div',
+      null,
+      title,
+      cta,
+      hero,
+      downLoadBtn
+    );
+
+    return React.createElement(
+      'div',
+      {
+        style: {
+          backgroundColor: '#FF6C37',
+          borderRadius: 10,
+          color: 'white',
+          margin: '10px 50px 50px',
+          padding: '40px 50px 80px',
+          filter: 'drop-shadow(8px 8px 10px rgba(0, 0, 0, 0.4))'
+        }
+      },
+      innerDownloadCard
+    );
+  }
+
   return React.createElement(
     'div',
     null,
-    React.createElement('b', null, (payload && payload.message) || ''),
-    `: ${nums}`
+    getStatsCard(),
+    getDownloadCard(),
+    getDescriptionCards()
   );
 };
 
-const App = props =>
+const App = () =>
   React.createElement(
     'div',
-    { style },
-    React.createElement(Hello, { ...props })
+    {
+      style: {
+        boxSizing: 'content-box',
+        fontFamily: 'Roboto,Tahoma,sans-serif',
+        paddingBottom: 100,
+        textAlign: 'center'
+      }
+    },
+    React.createElement(PM)
   );
 
-ReactDOM.render(
-  React.createElement(App, { style, nums: unique(numbers), head: 'hi' }, null),
-  document.getElementById('root')
-);
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
