d9b0c91466ca89f75103e1bc72278fb7119908a6
Brian Swisher <brian.swisher@getpostman.com>
2019-12-12 16:05:43 -0800

    [poc] pm_g (global) for header & footer

--- bff.js
+++ bff.js
@@ -2,9 +2,12 @@ const Bundler = require('parcel-bundler');
 const express = require('express');
 const fetch = require('node-fetch');
 const path = require('path');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 const app = express();
 const bundler = new Bundler('src/index.html', {});
+const host = 'https://www.getpostman.com';
 const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
@@ -23,8 +26,126 @@ app.set('json spaces', 2);
 
 app.use('/', express.static(path.join(__dirname, '..', 'dist')));
 
-app.get('/pm', (req, res) => {
-  fetch('https://www.getpostman.com/')
+app.get('/pm_g', (req, res) => {
+  fetch(host)
+    .then(
+      (r) => {
+        r.text()
+          .then(data => {
+            const { document } = (new JSDOM(data)).window;
+
+            const getHeader = () => {
+              const header = {type: 'header'};
+              const items = [];
+              const get = isTail => {
+                const nav = document.querySelector('[role="navigation"]');
+                const ul = [...nav.querySelectorAll('ul')];
+                const list = isTail && ul.pop() || ul.shift();
+
+                if (!header.media) {
+                  const media = nav.parentNode.querySelector('img').src;
+
+                  header.media = media.indexOf('http') === -1 && `${host}${media}` || media;
+                }
+
+                let count = 0;
+
+                [...list.querySelectorAll('li')].forEach(li => {
+                  const links = [...li.querySelectorAll('a')];
+                  const link = links.shift();
+                  const href = link.href;
+                  const lastChar = href[href.length - 1];
+                  const hasItems = lastChar === '#';
+
+                  if (href) {
+                    const count = items.push({
+                      title: link.textContent,
+                      url: href.indexOf('http') === -1 && `${host}${href}` || href
+                    });
+
+                    if (hasItems) {
+                      const lastItem = items[count - 1];
+
+                      lastItem.items = [];
+
+                      delete lastItem.url;
+
+                      [...link.parentNode.querySelector('div').querySelectorAll('a')].forEach(link => {
+                        const href = link.href;
+                        const url = href && (href.indexOf('http') === -1 && `${host}${href}` || href) || null;
+                        const data = {title: link.textContent};
+
+                        if (url) {
+                          data.url = url;
+                        }
+
+                        lastItem.items.push(data);
+                      });
+                    }
+                  }
+
+                  count++;
+                });
+
+                return count;
+              };
+
+              get();
+
+              header.items = items;
+              header.tail = get(true);
+
+              return header;
+            };
+
+            const getFooter = () => {
+              const footer = {type: 'footer', items: []};
+              const sections = [...document.querySelector('footer').querySelectorAll('section')];
+
+              sections.forEach(section => {
+                const data = {items: []};
+                const title = section.querySelector('p');
+
+                if (title) {
+                  data.title = title.textContent;
+
+                  const list = section.querySelector('ul');
+
+                  if (list) {
+                    [...list.querySelectorAll('a')].forEach(link => {
+                      const href = link.href;
+
+                      data.items.push({
+                        title: link.textContent,
+                        url: href.indexOf('http') === -1 && `${host}${href}` || href
+                      });
+                    });
+                  }
+                } else {
+                  [...section.querySelectorAll('a')].forEach(link => {
+                    const href = link.href;
+
+                    data.items.push({
+                      body: link.innerHTML,
+                      url: href.indexOf('http') === -1 && `${host}${href}` || href
+                    });
+                  });
+                }
+
+                footer.items.push(data);
+              });
+
+              return footer;
+            }
+
+            res.json([getHeader(), getFooter()]);
+          });
+      }
+    );
+});
+
+app.get('/poc', (req, res) => {
+  fetch(host)
     .then(
       (r) => {
         r.text()
