85eb0392801794abf5b81bb6984a6e5a5d527291
Brian Swisher <brian.swisher@getpostman.com>
2019-12-12 16:13:31 -0800

    [poc] /pm home page (data objects)

--- bff.js
+++ bff.js
@@ -10,6 +10,42 @@ const bundler = new Bundler('src/index.html', {});
 const host = 'https://www.getpostman.com';
 const PORT = process.env.PORT || 5000;
 
+function querySelectorMatchAll(dom, key, index) {
+  const patt = new RegExp(`[a-z,0-9]*[_,-]*${key}[_,-]*[a-z]*`, 'g');
+  return dom.querySelectorAll(`.${dom.innerHTML.match(patt).reduce((items, className) => {
+    if (items.indexOf(className) === -1) {
+      if (dom.querySelector(`.${className}`)) {
+        items.push(className);
+      }
+    }
+    return items;
+  }, [])[index || 0]}`)
+}
+
+
+function querySelectorMatch(dom, key) {
+  return querySelectorMatchAll.apply(dom, arguments)[0];
+}
+
+function findUpward(dom, className, loops) {
+  const MAX = loops || 5;
+
+  let target = dom;
+  let found;
+  let i;
+
+  for (i = 0; i < MAX; i++) {
+    target = target.parentNode;
+    if (target.className === className) {
+      found = target;
+
+      break;
+    }
+  }
+
+  return found;
+}
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
@@ -132,6 +168,273 @@ app.get('/pm_g', (req, res) => {
             }
 
             res.json([getHeader(), getFooter()]);
+          });
+      }
+    );
+});
+
+app.get('/pm', (req, res) => {
+  fetch(host)
+    .then(
+      (r) => {
+        r.text()
+          .then(data => {
+            const { document } = (new JSDOM(data)).window;
+
+            const getEvents = () => {
+              const events = {
+                type: 'event',
+                items: []
+              };
+
+              [...querySelectorMatchAll(document.body, 'events')].forEach(dom => {
+                events.items.push({date: dom.textContent, url: dom.parentNode.parentNode.href});
+              });
+
+              [...querySelectorMatchAll(document.body, 'events', 1)].forEach((dom, i) => {
+                if (!events.title) {
+                  events.title = findUpward(dom, 'container').textContent.split(events.items[i].date).shift();
+                }
+
+                events.items[i].location = dom.querySelector('p').textContent;
+                events.items[i].title = dom.textContent.split(events.items[i].location).pop();
+              });
+
+              return events;
+            };
+
+            const mkItems = type => ({
+              type,
+              items: []
+            });
+
+            const getMeta = () => {
+              return {
+                type: 'meta',
+                url: '/',
+                title: document.querySelector('title').textContent,
+              };
+            };
+
+            const getCards = () => {
+              const caseStudies = mkItems('caseStudy');
+              const descriptions = mkItems('describe');
+              const learnings = mkItems('learn');
+              const items = [];
+
+              [...querySelectorMatchAll(document.body, 'card')].forEach((card, i) => {
+                const text = card.textContent;
+                const hasQuote = text.indexOf('\"') !== -1;
+                const dom = card.querySelector('[href*="/"]');
+
+                items.push({});
+
+                if (dom) {
+                  const href = dom.href;
+                  const url = href.indexOf('http') === -1 && `${host}${href}` || href;
+
+                  items[i].url = url;
+                }
+
+                if (hasQuote && items[i].url && items[i].url.indexOf('.pdf') !== -1) {
+                  const head = text.split('"').shift();
+
+                  items[i].body = text.split(`${head}"`).pop().split('"').shift();
+                  items[i].title = text.split(items[i].body).shift().split('"').shift();
+                  items[i].author = text.replace(items[i].title, '').replace(items[i].body, '').split('"').pop();
+
+                  [...querySelectorMatchAll(card, 'card-media')].forEach(mediaCard => {
+                    const media = mediaCard.innerHTML.split(/src=./).pop().split('"').shift();
+
+                    items[i].media = media.indexOf('http') === -1 && `${host}${media}` || media;
+                  });
+
+                  if (!caseStudies.title) {
+                    caseStudies.title = findUpward(card , 'container').textContent.split(items[i].title).shift();
+                  }
+
+                  // caseStudy
+                  caseStudies.items.push(items[i]);
+                } else {
+
+                  const title = text.substring(0, text.indexOf(
+                    text.match(/[a-z,\.][A-Z]/)) + 1
+                  );
+                  const textNoTitle = text.split(title).pop();
+                  const body = textNoTitle.split('Learn').shift();
+
+                  items[i].title = title;
+                  items[i].body = body;
+
+                  if (Boolean(textNoTitle.match(/Learn/))) {
+
+                    // learn
+                    learnings.items.push(items[i]);
+
+                    if (!i) {
+                      const text = findUpward(card, 'container').textContent.split(items[i].title).shift();
+                      const body = text.split('Postman?').pop();
+                      const title = text.split(body).shift();
+
+                      learnings.items.unshift({
+                        title,
+                        body
+                      });
+                    }
+
+                  } else {
+
+                    if (!descriptions.title) {
+                      descriptions.title = findUpward(card, 'container').textContent.split(items[i].title).shift();
+                    }
+
+                    // describe
+                    descriptions.items.push(items[i]);
+
+                  }
+                }
+              });
+
+              return [caseStudies, learnings, descriptions];
+            };
+
+            const getStats = (ctaInstead) => {
+              const metrics = {type: 'metric', items: []};
+              const section = document.querySelector('section');
+              const stats = [...section.querySelectorAll('p')].slice(0, 7);
+              const cta = stats.shift();
+              const MAX = stats.length;
+
+              if (ctaInstead) {
+                const href = section.querySelector('[title^="Download"]').href;
+
+                return {
+                  type: 'cta',
+                  title: section.textContent.split(cta.textContent).shift(),
+                  body: cta.textContent,
+                  url: href.indexOf('http') === -1 && `${host}${href}` || href
+                };
+              }
+
+              let i;
+
+              for (i = 0; i < MAX; i = i + 2) {
+                metrics.items.push({
+                  title: stats[i + 1].innerHTML,
+                  body: stats[i].innerHTML
+                });
+              }
+
+              return metrics;
+            }
+
+            const getCTA = () => getStats(true);
+
+            const getRoles = (useCasesInstead) => {
+              const roles = {type: 'role', items: []};
+              const section = document.querySelectorAll('section')[2];
+              const items = [...section.querySelectorAll('p')].slice(0, 4);
+              const rolesDescription = items.shift();
+              const MAX = items.length;
+
+              if (useCasesInstead) {
+                const container = [...section.querySelectorAll('.container')].pop();
+                const cases = [...container.querySelectorAll('p')];
+                const body = cases.shift();
+                const bodyText = body.textContent;
+                const items = [];
+                const MAX_CASES = cases.length;
+
+                let i;
+
+                for (i = 0; i < MAX_CASES; i = i + 2) {
+                  const href = cases[i].querySelector('a').href;
+
+                  items.push({
+                    title: cases[i].textContent,
+                    body: cases[i + 1].textContent,
+                    url: href.indexOf('http') === -1 && `${host}${href}` || href
+                  });
+                }
+
+                return {
+                  type: 'useCases',
+                  title: container.textContent.split(bodyText).shift(),
+                  body: bodyText,
+                  items
+                };
+              }
+
+              let i;
+
+              for (i = 0; i < MAX; i++) {
+                const href = items[i].querySelector('a').href;
+
+                if (!roles.title) {
+                  roles.body = rolesDescription.textContent;
+                  roles.title = section.textContent.split(roles.body).shift();
+                }
+
+                roles.items.push({
+                  title: items[i].textContent,
+                  url: href.indexOf('http') === -1 && `${host}${href}` || href
+                });
+              }
+
+              return roles;
+            }
+
+            const getUseCases = () => getRoles(true);
+
+            const getNetwork = () => {
+              const network = {type: 'network'};
+              const section = document.querySelectorAll('section')[5];
+              const body = [...section.querySelectorAll('p')].shift();
+              const bodyText = body.textContent;
+              const media = section.querySelector('img').outerHTML.split('src="').pop().split('"').shift();
+              const link = [...section.querySelectorAll('a')].pop();
+              const href = link.href;
+
+              network.title = section.textContent.split(bodyText).shift();
+              network.body = bodyText;
+              network.media = media.indexOf('http') === -1 && `${host}${media}` || media;
+              network.url = href.indexOf('http') === -1 && `${host}${href}` || href;
+              network.cta = link.textContent;
+
+              return network;
+            };
+
+            const getCommunity = () => {
+              const community = {type: 'community'};
+              const section = document.querySelectorAll('section')[6];
+
+              let lines = [...section.querySelectorAll('p')];
+
+              const link = lines.pop().querySelector('a');
+              const href = link.href;
+              const items = lines.map(line => line.textContent);
+              const media = section.querySelector('img').outerHTML.split('src="').pop().split('"').shift();
+
+              community.title = section.textContent.split(lines[0].textContent).shift();
+              community.cta = link.textContent;
+              community.url = href.indexOf('http') === -1 && `${host}${href}` || href;
+              community.media = media.indexOf('http') === -1 && `${host}${media}` || media;
+              community.items = items;
+
+              return community;
+            };
+
+            res.json([
+              getMeta(),
+              getCTA(),
+              getStats(),
+              getRoles(),
+              getUseCases(),
+              ...getCards(),
+              getNetwork(),
+              getCommunity(),
+              getEvents()
+            ]);
           });
       }
     );
